32a09f5e3d14df3ba9f3b67f246a3c51f7709362
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-11-26 13:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name , age) {
+		this._name = name;
+		this._age = age;
+	}
+	// Getter for the name
+	get name() {
+		return this._name
+	}
 
-class Student extends Person {}
+	// Setter for tbe name
+	set age(age) {
+		if(typeof age === "number" && age > 0) {
+			this._age = age;
+		} else {
+			console.log("Please provide a valid age")
+		}
+	}
 
-class Teacher extends Person {}
+	// get name
+
+	get age() {
+		return this._age
+	}
+}
+
+class Student extends Person {
+
+	study() {
+		console.log(`${this.name} is studing`)
+	}
+	
+}
+
+class Teacher extends Person {
+
+	teach() {
+		console.log(`${this.name} is teaching`)
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
